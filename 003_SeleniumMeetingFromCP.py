8092793935765fea86b4ee36f3984533f81ddb96
MaximSidorkin <sidorkin.maxim@yahoo.com>
2016-12-27 12:36:42 +0300

    + patch 2712 for 003 test ++ incr. waiting for load DT = 40 sec.

--- 003_SeleniumMeetingFromCP.py
+++ 003_SeleniumMeetingFromCP.py
@@ -13,7 +13,7 @@ import HTMLTestRunner
 driver = webdriver.Firefox()
 driver.get("https://dev.eor.gosapi.ru/site/login")
 driver.maximize_window()
-wait = WebDriverWait(driver, 20)
+wait = WebDriverWait(driver, 40)
 
 class ASeleniumLogin_1(unittest.TestCase):
     def test_001_LoginInEORDev(self):
