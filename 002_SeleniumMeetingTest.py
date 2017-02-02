30bcc94d85cbbb53cde34325d3e84b39b4a8f8a1
MaximSidorkin <sidorkin.maxim@yahoo.com>
2017-02-02 14:16:29 +0300

    + patch for 002 test ++ add test for cross-check for users ipad/login

--- 002_SeleniumMeetingTest.py
+++ 002_SeleniumMeetingTest.py
@@ -16,7 +16,7 @@ driver = webdriver.Firefox()
 driver.get("https://dev.eor.gosapi.ru/site/login")
 driver.maximize_window()
 time.sleep(2)
-wait = WebDriverWait(driver, 50)
+wait = WebDriverWait(driver, 120)
 driver.implicitly_wait(50)
 
 class ASeleniumLogin_1(unittest.TestCase):
@@ -28,12 +28,12 @@ class ASeleniumLogin_1(unittest.TestCase):
         elem = driver.find_element_by_id("LoginForm_password")
         elem.send_keys("ipad")
         elem.send_keys(Keys.RETURN)
-        print('тест №1 - логинимся в систему')
+        print(' логинимся в систему')
 
     def test_002_Not500or404andLoginIsVisible(self):
         _ = wait.until(EC.element_to_be_clickable((By.CLASS_NAME, 'hidden-xs')))
         assert "ЭОР - Error" not in driver.title  # проверка на 500/404 ошибку
-        print('тест №2 - проверка на 404 и 500 ошибку после ввода логина/пароля')
+        print(' проверка на 404 и 500 ошибку после ввода логина/пароля')
         try:
             driver.find_element_by_class_name('hidden-xs')
         except:
@@ -47,12 +47,12 @@ class ASeleniumLogin_1(unittest.TestCase):
         schedul = driver.find_element_by_link_text("Расписание")
         schedul.click()
         _ = wait.until(EC.element_to_be_clickable((By.CLASS_NAME, 'add-meeting')))
-        print('тест №3 - переход в раздел "Расписание"')
+        print(' переход в раздел "Расписание"')
 
     def test_004_ClickCreateMeeting(self):
         time.sleep(4)
         crMeeting = driver.find_element_by_xpath("//div[@id='bs-example-navbar-collapse-3']/div[5]/div[2]").click()
-        print('тест №4 - нажимаем кнопку "Создать" на открывшейся форме')
+        print(' нажимаем кнопку "Создать" на открывшейся форме')
 
     def test_005_FillingMeetingForm(self):
         time.sleep(3)
@@ -75,12 +75,12 @@ class ASeleniumLogin_1(unittest.TestCase):
         time.sleep(2)
         triggerOffer = driver.find_element_by_xpath("//form[@id='meetings-form']/div[13]/div/div/div/span").click()
         driver.save_screenshot('C:\PyTest\inish.png')
-        print('тест №5 - заполняем форму создания совещания')
+        print(' заполняем форму создания совещания')
 
     def test_006_Confirm(self):
         time.sleep(2)
         driver.find_element_by_name('yt0').click()
-        print('тест №6 - подтверждаем создание совещание')
+        print(' подтверждаем создание совещание')
         try:
             popUp = driver.find_element_by_css_selector('div.toast-message')
             print('Всплывающее уведомлние: "',popUp.text, '" - выведено')
@@ -103,7 +103,7 @@ class ASeleniumLogin_1(unittest.TestCase):
         time.sleep(1)
         missionButton = driver.find_element_by_name('yt0').click()
         time.sleep(2)
-        print('тест №7 - находим созданное совещание и редактируем его')
+        print(' находим созданное совещание и редактируем его')
 
         time.sleep(4)
         driver.implicitly_wait(10)
@@ -132,10 +132,10 @@ class ASeleniumLogin_1(unittest.TestCase):
         time.sleep(1)
         driver.find_element_by_xpath('//div/div[3]/span[2]').click()
         time.sleep(2)
-        print('тест №8 - создаем поучение и заполняем его форму')
+        print(' создаем поучение и заполняем его форму')
 
     def test_009_Close(self):
-        print('тест №9 - подтверждаем создание поручения/закрываем форму')
+        print(' подтверждаем создание поручения/закрываем форму')
 
     def test_010_NewCommissionPlus(self):
         time.sleep(1)
@@ -148,7 +148,7 @@ class ASeleniumLogin_1(unittest.TestCase):
         ASeleniumLogin_1.test_008_FillingCommissionForm(self)
         time.sleep(2)
         driver.find_element_by_id('btn_close').click()
-        print('тест №10 - создаем поручение непосредственно из паспорта совещания путем нажатия кнопки "+"')
+        print(' создаем поручение непосредственно из паспорта совещания путем нажатия кнопки "+"')
 
     def test_011_DelMeeting(self):
         time.sleep(2)
@@ -160,7 +160,7 @@ class ASeleniumLogin_1(unittest.TestCase):
         time.sleep(3)
         driver.implicitly_wait(15)
         driver.find_element(By.XPATH, "//div[3]/div/button").click()
-        print('тест №11 - удаляем созданное совещание')
+        print(' удаляем созданное совещание')
 
     def test_012_AllDayMeeting(self):
         time.sleep(2)
@@ -172,7 +172,7 @@ class ASeleniumLogin_1(unittest.TestCase):
         time.sleep(2)
         driver.find_element_by_name('yt0').click()
         time.sleep(2)
-        print('тест №12 - создаем совещание на весь день')
+        print(' создаем совещание на весь день')
 
     def test_013_DelAllDayMeeting(self):
         driver.find_element_by_css_selector('span.fc-title').click()
@@ -180,7 +180,7 @@ class ASeleniumLogin_1(unittest.TestCase):
         driver.find_element(By.XPATH, ".//*[text()='Удалить']/..").click()
         time.sleep(2)
         driver.find_element(By.XPATH, ".//*[text()='Да']/..").click()
-        print('тест №13 - удаляем совещание созданное на весь день')
+        print(' удаляем совещание созданное на весь день')
 
     def test_014_CreateMeetingFromDT(self):
         time.sleep(2)
@@ -204,20 +204,20 @@ class ASeleniumLogin_1(unittest.TestCase):
         time.sleep(2)
         # нажимаем кнопку создать
         driver.find_element_by_name('yt0').click()
-        print('тест №14 - создаем совещание с рабочего стола')
+        print(' создаем совещание с рабочего стола')
 
     def test_015_GotoScheduller(self):
         time.sleep(4)
         driver.find_element_by_link_text("Расписание").click()
         _ = wait.until(EC.element_to_be_clickable((By.CLASS_NAME, 'add-meeting')))
-        print('тест №15 - переходим в раздел "Расписание"')
+        print(' переходим в раздел "Расписание"')
 
     def test_016_SearchDTMeeting(self):
         time.sleep(5)
         newMeet = driver.find_element_by_xpath("//span[. = '19:08 - 20:08' ]")
         newMeet.click()
         time.sleep(4)
-        print('тест №16 - находим только что созданное совещение')
+        print(' находим только что созданное совещение')
 
     def test_017_AddCommission(self):
         driver.find_element_by_css_selector('i.fa.fa-plus').click()
@@ -225,7 +225,7 @@ class ASeleniumLogin_1(unittest.TestCase):
         ASeleniumLogin_1.test_008_FillingCommissionForm(self)
         time.sleep(2)
         driver.find_element_by_id('btn_close').click()
-        print('тест №17 - создаем поучение и заполняем его форму')
+        print(' создаем поучение и заполняем его форму')
 
     def test_018_CreateMeetingCopy(self):
         time.sleep(3)
@@ -237,14 +237,14 @@ class ASeleniumLogin_1(unittest.TestCase):
         driver.find_element(By.XPATH, ".//*[text()='Создать копию']/..").click()
         time.sleep(3)
         driver.find_element_by_name('yt0').click()
-        print('тест №19 - переходим в раздел "Расписание", находим совещание, и создаём его копию')
+        print(' переходим в раздел "Расписание", находим совещание, и создаём его копию')
 
     def test_019_Comment(self):
         time.sleep(3)
         driver.implicitly_wait(10)
         driver.find_element_by_xpath("//span[. = '19:08 - 20:08' ]").click()
         time.sleep(3)
-        print('тест №20 - открываем паспорт совещание')
+        print(' открываем паспорт совещание')
 
     def test_020_printComment(self):
         driver.find_element_by_name('notes').click()
@@ -254,17 +254,195 @@ class ASeleniumLogin_1(unittest.TestCase):
         time.sleep(4)
         commentText = driver.find_element_by_xpath('//textarea').send_keys(' Hello, World! ')
         saveBtn = driver.find_element_by_css_selector('input.btn').click()
-        print('тест №21 - создаем комментарий')
+        print(' создаем комментарий')
 
     def test_021_editComment(self):
         time.sleep(2)
         ASeleniumLogin_1.test_020_printComment(self)
-        print('тест №22 - редактируем комментарий')
+        print(' редактируем комментарий')
 
     def test_022_closeComment(self):
         time.sleep(1)
         driver.find_element_by_xpath("//div[@id='notes']/div/div/div[2]/a/i").click()
-        print('тест №23 - закрываем форму комментария')
+        print(' закрываем форму комментария')
+
+        # проверка передачи изменений в совещании для пользователя А(+ outlook acc.) и Б (- outlook acc.)
+        # пользователь А - ipad
+        # пользователь Б - login
+    def test_023_crMeetForUserA(self):
+        time.sleep(1)
+        driver.find_element_by_xpath("//div[@id='bs-example-navbar-collapse-3']/div[5]/div[2]").click()
+        time.sleep(3)
+        name = driver.find_element_by_id('MeetingsData_S_NAME').send_keys('Пользователь А')
+        responsibleName = driver.find_element_by_xpath('//div[8]/div/span/span/span/span[2]').click()
+        time.sleep(2)
+        responsibleName = driver.find_element_by_xpath('//body[@id="ui-id-1"]/span/span/span/input').send_keys('Афанасьев' + Keys.ENTER)
+        meetingDateB = driver.find_element_by_id('MeetingsData_D_START').clear()
+        meetingDateB = driver.find_element_by_id('MeetingsData_D_START').send_keys('20:22' + Keys.ENTER)
+        meetingDateE = driver.find_element_by_id('MeetingsData_D_END').clear()
+        meetingDateE = driver.find_element_by_id('MeetingsData_D_END').send_keys('20:52' + Keys.ENTER)
+        time.sleep(2)
+        driver.find_element_by_name('yt0').click()
+
+        print(' Проверка передачи изменений в совещании для\n '
+              'пользователя А(+ outlook acc.) и Б (- outlook acc.) \n'
+              '- Заполняем форму создания совещания для пользоватлея А (ipad)')
+
+    def test_024_gotoOutlook(self):
+        time.sleep(1)
+        driver.set_page_load_timeout(20)
+        driver.get("https://owa.mos.ru/")
+        print(" Переходим в Outlook")
+        wait.until(EC.presence_of_element_located((By.ID, "username")))
+        elem = driver.find_element_by_id("username")
+        elem.send_keys("MarenovaTE")
+        elem = driver.find_element_by_id("password")
+        elem.send_keys("rTZmYVbx")
+        elem.send_keys(Keys.RETURN)
+
+    def test_025_openCalendar(self):
+        driver.set_page_load_timeout(20)
+        #_ = wait.until(EC.element_to_be_clickable((By.XPATH, './/*[text()="Календарь"]/..')))
+        driver.find_element(By.XPATH, ".//*[text()='Календарь']/..").click()
+        try:
+            driver.find_element_by_xpath(".//*[text()='Пользователь А Отв.: Афанасьев В.П.']/..").click()
+            print(' Совещание созданное в ЭОР найдено')
+        except:
+            print(' аутглюк завис')
+        time.sleep(2)
+        driver.find_element_by_xpath(".//*[text()='ИЗМЕНИТЬ']/..").click()
+
+    def test_026_ConfirmField(self):
+        wait.until(EC.element_to_be_clickable((By.XPATH, '//div[2]/div[2]/div[2]/div/div/input')))
+        try:
+            _ = driver.find_element_by_xpath('//div[2]/div[2]/div[2]/div/div/input').text == 'Пользователь А Отв.: Афанасьев В.П.'
+            print(' Текст названия и ответственного соответствует')
+        except:
+            self.fail(print(' ОШИБКА!'
+                            '\nТекст названия и ответственного соответствует'))
+        try:
+            _ = driver.find_element_by_xpath('//li/div[2]/div/div/div/input').text == '20:22'
+            print(' Время соответствует')
+        except:
+            self.fail(print(' ОШИБКА!\n'
+                            'Время совещания не соответствует'))
+
+    def test_027_reLogInEOR(self):
+        time.sleep(3)
+        driver.get("https://dev.eor.gosapi.ru/site/login")
+        wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, 'span.caret')))
+        driver.find_element_by_css_selector('span.caret').click()
+        driver.find_element_by_link_text('Выход').click()
+        wait.until(EC.element_to_be_clickable((By.ID, 'LoginForm_username')))
+        elem = driver.find_element_by_id("LoginForm_username")
+        elem.send_keys("login")
+        elem = driver.find_element_by_id("LoginForm_password")
+        elem.send_keys("login")
+        elem.send_keys(Keys.RETURN)
+        print(' Перезаходим в систему под пользователем login/login ')
+
+    def test_028_gotoMeet(self):
+        driver.find_element_by_link_text("Расписание").click()
+        _ = wait.until(EC.element_to_be_clickable((By.CLASS_NAME, 'add-meeting')))
+        print(' Переход в раздел "Расписание" ')
+
+    def test_029_searchMeet(self):
+        time.sleep(1)
+        try:
+            driver.find_element_by_xpath("//span[. = '20:22 - 20:52' ]")
+            print(' Совещание найдено')
+        except:
+            self.fail(print(' ОШИБКА!\n'
+                            'совещание не найдено'))
+
+    def test_030_reLogInEOR(self):
+        time.sleep(1)
+        wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, 'span.caret')))
+        driver.find_element_by_css_selector('span.caret').click()
+        driver.find_element_by_link_text('Выход').click()
+        wait.until(EC.element_to_be_clickable((By.ID, 'LoginForm_username')))
+        elem = driver.find_element_by_id("LoginForm_username")
+        elem.send_keys("Ipad")
+        elem = driver.find_element_by_id("LoginForm_password")
+        elem.send_keys("ipad")
+        elem.send_keys(Keys.RETURN)
+        print(' Перезаходим в систему под пользователем ipad/ipad ')
+
+    def test_031_gotoMeet(self):
+        ASeleniumLogin_1.test_028_gotoMeet(self)
+        driver.find_element_by_xpath("//span[. = '20:22 - 20:52' ]").click()
+        print(' В разеде "Расписание" находим совещание')
+
+    def test_032_editMeet(self):
+        wait.until(EC.element_to_be_clickable((By.XPATH, '//button[2]')))
+        driver.find_element_by_xpath("//button[2]").click()
+        meetingDateB = driver.find_element_by_id('MeetingsData_D_START').clear()
+        meetingDateB = driver.find_element_by_id('MeetingsData_D_START').send_keys('20:23' + Keys.ENTER)
+        meetingDateE = driver.find_element_by_id('MeetingsData_D_END').clear()
+        meetingDateE = driver.find_element_by_id('MeetingsData_D_END').send_keys('20:52' + Keys.ENTER)
+        driver.find_element_by_name('yt0').click()
+        print(" Редактируем совещание - изменяем время")
+
+    def test_033_reLogInEOR(self):
+        time.sleep(1)
+        wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, 'span.caret')))
+        driver.find_element_by_css_selector('span.caret').click()
+        driver.find_element_by_link_text('Выход').click()
+        wait.until(EC.element_to_be_clickable((By.ID, 'LoginForm_username')))
+        elem = driver.find_element_by_id("LoginForm_username")
+        elem.send_keys("login")
+        elem = driver.find_element_by_id("LoginForm_password")
+        elem.send_keys("login")
+        elem.send_keys(Keys.RETURN)
+        print(' Перезаходим в систему под пользователем login/login ')
+
+    def test_034_gotoMeet(self):
+        time.sleep(1)
+        ASeleniumLogin_1.test_028_gotoMeet(self)
+        #print("тест №34 - Переходим в раздел 'Расписание'")
+
+    def test_035_searchMeet(self):
+        time.sleep(1)
+        try:
+            driver.find_element_by_xpath("//span[. = '20:23 - 20:52' ]")
+            print(' Совещание найдено')
+        except:
+            self.fail(print(' ОШИБКА!\n'
+                            'совещание не найдено'))
+
+    def test_036_gotoOutlook(self):
+        time.sleep(1)
+        driver.get("https://owa.mos.ru/")
+        driver.set_page_load_timeout(20)
+        #25
+        ASeleniumLogin_1.test_025_openCalendar(self)
+        wait.until(EC.element_to_be_clickable((By.XPATH, '//div[2]/div[2]/div[2]/div/div/input')))
+        driver.find_element_by_xpath('//li/div[2]/div/div/div/input').clear()
+        driver.find_element_by_xpath('//li/div[2]/div/div/div/input').send_keys('20:24')
+        time.sleep(2)
+        driver.find_element(By.XPATH, ".//*[text()='СОХРАНИТЬ']/..").click()
+        print(' Переходим в Outlook, раздел календарь редактируем совещание')
+
+    def test_037_gotoEOR(self):
+        print('Запускаем синхронизатор')
+        # синхронизируем outlook - ЕОР
+        time.sleep(4)
+        driver.get("https://dev.eor.gosapi.ru/ewsup")
+        _ = wait.until(EC.element_to_be_clickable((By.ID, 'btn-ewsup')))
+        driver.find_element_by_id('btn-ewsup').click()
+        try:
+            wait.until(EC.presence_of_element_located((By.XPATH, "//*[contains(text(), 'COMPLETE')]")))
+            print(' Синхронизация прошла успешно ')
+        except:
+            self.fail(print(' \n \n ОШИБКА СИНХРОНИЗАЦИИ \n \n'))
+         #переходим в ЭОР для login и проверяем время совещания
+        ASeleniumLogin_1.test_028_gotoMeet(self)
+        try:
+            driver.find_element_by_xpath("//span[. = '20:24 - 20:53' ]")
+            print(' Совещание найдено')
+        except:
+            self.fail(print(' ОШИБКА!\n'
+                            'совещание не найдено'))
 
 if __name__ == '__main__':
     suite = unittest.TestSuite()
