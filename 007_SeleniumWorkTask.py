ef7dd8917422e3e4c88aefe862db0b81fb6a74d3
MaximSidorkin <sidorkin.maxim@yahoo.com>
2017-05-15 10:59:07 +0300

    + back in DEV

--- 007_SeleniumWorkTask.py
+++ 007_SeleniumWorkTask.py
@@ -223,7 +223,7 @@ class ASeleniumLogin_1(unittest.TestCase):
 
         _ = wait.until(EC.element_to_be_clickable((By.ID, 'LoginForm_username')))
         elem = driver.find_element_by_id("LoginForm_username")
-        elem.send_keys("Selenium_01")
+        elem.send_keys("Selenium_1")
         elem = driver.find_element_by_id("LoginForm_password")
         elem.send_keys("123")
         elem.send_keys(Keys.RETURN)
